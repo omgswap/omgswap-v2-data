07da37a12833ceba872b379c1c1c2308a77fd91e
Sol Proc <info@kp2r.network>
2020-12-14 13:06:01 +0600

    updatesub

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -4,7 +4,7 @@ import { HttpLink } from 'apollo-link-http'
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://api.thegraph.com/subgraphs/name/bitbd83/omegasub'
+    uri: 'https://api.thegraph.com/subgraphs/name/bitbd83/omegaswap2'
   }),
   cache: new InMemoryCache(),
   shouldBatch: true
